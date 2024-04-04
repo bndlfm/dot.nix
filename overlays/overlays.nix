7595c2f8bc45f717cdaf053f776b11f9df52a345
bndlfm <firefliesandlightningbugs@gmail.com>
2024-04-04 09:02:02 -0500

    trying to reactivate my overlays

--- overlays/overlays.nix
+++ overlays/overlays.nix
@@ -1,6 +1,6 @@
 self: super: {
-  firefox-devedition = super.firefox-devedition.overrideAttrs (oldAttrs: {
-    patches = oldAttrs.patches ++ [
+  firefox-devedition = super.firefox-devedition-unwrapped.overrideAttrs (oldAttrs: {
+    patches = (oldAttrs.patches or []) ++ [
       (super.fetchpatch {
         url = "https://raw.githubusercontent.com/bndlfm/ffMemoryCache/main/ext-tabs.js.patch";
         sha256 = "sha256-Kp21VA4d9RRdP3wwyiu54E4w5b3qbcZBz5YlqAM1Q7I=";
