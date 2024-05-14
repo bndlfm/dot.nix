c5c7692873a0bff5184745422cf39468b0acc69f
bndlfm <firefliesandlightningbugs@gmail.com>
2024-05-13 23:44:23 -0500

    funkwhale initial setup

---
+++ services/funkwhale.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, ... }:{
+  nixpkgs.overlays = [ funkwhale.overlay ];
+
+  services = {
+    fail2ban.enable = true;
+    funkwhale = {
+      enable = true;
+      hostname = "placeholder";
+      defaultFromEmail = "noreply@placeholder";
+      protocol = "https";
+      #forceSSL = false; # uncomment when LetsEncrypt needs to check "http:"
+      api = {
+        djangoSecretServiceKeyFile = "${djangoSecretFile}";
+      };
+    };
+    nginx.clientMaxBodySize = "100m";
+  };
+  security.acme = {
+    email = "placeholder";
+    acceptTerms = true;
+  };
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
+}
