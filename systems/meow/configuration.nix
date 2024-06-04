1549dcece468522e83b09866c4a6206ac9a2251b
bndlfm <firefliesandlightningbugs@gmail.com>
2024-06-04 11:37:30 -0500

    okay this is gonna suck

--- systems/meow/configuration.nix
+++ systems/meow/configuration.nix
@@ -18,19 +18,12 @@
       trusted-substituters = [
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
-        "https://ai.cachix.org"
-        "https://nix-gaming.cachix.org"
-        #"https://chaotic-nyx.cachix.org"
-        #"https://ezkea.cachix.org"
+        #"https://ai.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="
-        "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-        #"nyx.chaotic.cx-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
-        #"chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
-        #"ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI="
+        #"ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="
       ];
     };
   };
@@ -273,16 +266,16 @@
     #  ];
     #};
 
-    "/media" = {
-      device = "/dev/disk/by-uuid/fe4494de-0116-404f-9c8a-5011115eedbf";
-      fsType = "btrfs";
-      options = [
-        "subvol=@media"
-        "noatime"
-        "nodiratime"
-        "discard"
-      ];
-    };
+    #"/media" = {
+    #  device = "/dev/disk/by-uuid/fe4494de-0116-404f-9c8a-5011115eedbf";
+    #  fsType = "btrfs";
+    #  options = [
+    #    "subvol=@media"
+    #    "noatime"
+    #    "nodiratime"
+    #    "discard"
+    #  ];
+    #};
   };
 
 
