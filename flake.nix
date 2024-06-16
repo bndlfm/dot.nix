486865f2f3af3fd74e26cd0d17cc96350cc26618
bndlfm <firefliesandlightningbugs@gmail.com>
2024-06-16 03:48:29 -0500

    fixing cachix and setting up genshin

--- flake.nix
+++ flake.nix
@@ -1,16 +1,4 @@
 {
-  nixConfig = {
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-      "https://ezkea.cachix.org" # AN ANIME GAME LAUNCHER
-    ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "ezeak.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI=" # AN ANIME GAME LAUNCHER
-    ];
-  };
-
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
