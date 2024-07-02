6b3e95b013bd680390602f27ffa0cdec27667880
bndlfm <firefliesandlightningbugs@gmail.com>
2024-07-02 01:16:45 -0500

    still hacking away at this derivation

--- packages/hyper-shell/flake.nix
+++ packages/hyper-shell/flake.nix
@@ -18,6 +18,42 @@
           distro
         ]);
 
+        hyper-shell = pkgs.stdenv.mkDerivation rec {
+          pname = "hyper-shell";
+          version = "0.1.0";
+
+          src = pkgs.fetchFromGitHub {
+            owner = "kirabase";
+            repo = "hyper-shell";
+            rev = "master";
+            hash = "sha256-ou9yKTR5PGsTR5LMxBIxRFxWFXYe739WXySnbzSSnTQ=";
+          };
+
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+
+          installPhase = /* sh */ ''
+            mkdir -p $out/bin $out/share/${pname}
+            cp -R ./* $out/share/${pname}
+            cp ${configFile} $out/share/${pname}/config.ini
+
+            for file in $out/share/${pname}/*.py; do
+              if [ -f "$file" ]; then
+                filename=$(basename "$file")
+                makeWrapper ${pythonWithPackages}/bin/python $out/bin/''${filename%.py} \
+                  --add-flags "$out/share/${pname}/$filename" \
+                  --prefix PYTHONPATH : $out/share/${pname}
+              fi
+            done
+          '';
+
+          meta = with pkgs.lib; {
+            description = "A shell environment powered by AI";
+            homepage = "https://github.com/kirabase/hyper-shell";
+            license = licenses.mit;  # Adjust if the license is different
+            maintainers = with maintainers; [ ];  # Add maintainers if known
+          }; 
+        }; 
+
         configFile = pkgs.writeTextFile {
           name = "config.ini";
           text = ''
@@ -42,81 +78,51 @@
           '';
         };
 
-        launcherScript = pkgs.writeShellScriptBin "hyper-shell" ''
-          #!/usr/bin/env bash
-          exec ${pythonWithPackages}/bin/python $out/share/hyper-shell/dodo.py "$@"
-        '';
-
-        fishConfig = { config, programs, ... }: {
-          programs.fish = {
-            shellAliases = programs.fish.shellAliases ++ {
-              "dodo" = "python dodo.py";
-              "hs" = "hyper-shell";
-            };
-            interactiveShellInit = ''
-            '';
-          };
-          #home.file.".bin/hyper-shell".source = "${hyper-shell}/bin/hyper-shell";
-        };
-
-        hyper-shell = pkgs.stdenv.mkDerivation rec {
-          pname = "hyper-shell";
-          version = "0.1.0";  # You may want to update this to the latest version
-
-          src = pkgs.fetchFromGitHub {
-            owner = "kirabase";
-            repo = "hyper-shell";
-            rev = "master";
-            hash = "sha256-ou9yKTR5PGsTR5LMxBIxRFxWFXYe739WXySnbzSSnTQ=";
-          };
-
-          nativeBuildInputs = [ pkgs.makeWrapper ];
-
-          installPhase = ''
-            mkdir -p $out/bin $out/share/${pname}
-            cp -R ./* $out/share/${pname}
-            cp ${configFile} $out/share/${pname}/config.ini
-
-            # Create shell script
-            #cat > $out/bin/hyper-shell <<EOF
-            ##!/usr/bin/env bash
-            #exec ${pythonWithPackages}/bin/python $out/share/${pname}/dodo.py "$@"
-            #EOF
-
-            #chmod +x $out/bin/hyper-shell
-
-            #makeWrapper $out/bin/hyper-shell $out/bin/hyper-shell \
-            #  --prefix PYTHONPATH : $out/share/${pname} \
-
-            install -Dm755 ${launcherScript}/bin/hyper-shell $out/bin/hyper-shell
-
-            for file in $out/share/${pname}/*.py; do
-              if [ -f "$file" ]; then
-                filename=$(basename "$file")
-                makeWrapper ${pythonWithPackages}/bin/python $out/bin/''${filename%.py} \
-                  --add-flags "$out/share/${pname}/$filename" \
-                  --prefix PYTHONPATH : $out/share/${pname}
-              fi
-            done
-          '';
-
-          meta = with pkgs.lib; {
-            description = "A shell environment powered by AI";
-            homepage = "https://github.com/kirabase/hyper-shell";
-            license = licenses.mit;  # Adjust if the license is different
-            maintainers = with maintainers; [ ];  # Add maintainers if known
-          };
-        };
-
 
       in {
-        packages.default = hyper-shell;
 
-        homeManagerModules.default = fishConfig;
+        packages.default = hyper-shell;
 
         # Optionally, you can also add a devShell
         devShells.default = pkgs.mkShell {
           buildInputs = [ hyper-shell pythonWithPackages ];
         };
-      });
+      }
+    );
 }
+
+
+
+#NOTE: OLD / NOT WORKING
+  #installPhase = ''
+  #  # Create shell script
+  #  cat > $out/bin/hyper-shell <<EOF
+  #  #!/usr/bin/env bash
+  #  exec ${pythonWithPackages}/bin/python $out/share/${pname}/dodo.py "$@"
+  #  EOF
+  #
+  #  chmod +x $out/bin/hyper-shell
+  #
+  #  makeWrapper $out/bin/hyper-shell $out/bin/hyper-shell \
+  #    --prefix PYTHONPATH : $out/share/${pname} \
+  #  install -Dm755 ${launcherScript}/bin/hyper-shell $out/bin/hyper-shell
+  #'';
+
+#launcherScript = pkgs.writeShellScriptBin "hyper-shell" ''
+  #  #!/usr/bin/env bash
+  #  exec ${pythonWithPackages}/bin/python $out/share/hyper-shell/dodo.py "$@"
+#'';
+
+#fishConfig = { config, programs, ... }: {
+  #  programs.fish = {
+  #    shellAliases = programs.fish.shellAliases ++ {
+  #      "dodo" = "python dodo.py";
+  #      "hs" = "hyper-shell";
+  #    };
+  #    interactiveShellInit = ''
+  #    '';
+  #  };
+  #  #home.file.".bin/hyper-shell".source = "${hyper-shell}/bin/hyper-shell";
+#};
+
+#homeManagerModules.default = fishConfig;
