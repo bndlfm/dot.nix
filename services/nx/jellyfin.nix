45ec605f1df11c2e5d9bb36deb117e48d0c352d4
bndlfm <firefliesandlightningbugs@gmail.com>
2024-07-23 06:37:11 -0500

    update and wow lot of changes i guess, setting up jellyfin as a nix service, installing wavey-launcher b/c zzz doesn't work in linux well, fixing spicetify and changing flake repo

---
+++ services/nx/jellyfin.nix
@@ -0,0 +1,10 @@
+{ ... }:{
+  services.jellyfin = {
+    enable = true;
+    openFirewall = true;
+    dataDir = "/media/.jellyfin/data/";
+    cacheDir = "/media/.jellyfin/cache/";
+    configDir = "/media/.jellyfin/config/";
+  };
+  services.jellyseerr.enable = false;
+}
