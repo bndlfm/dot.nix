b6900695ede6851f97ac147033818ee05527f4bd
bndlfm <firefliesandlightningbugs@gmail.com>
2025-02-04 23:39:44 -0600

    trying out module system

--- modules/nixos/hyprland.mod.sys.nix
+++ modules/nixos/hyprland.mod.sys.nix
@@ -3,11 +3,11 @@
     neko.hyprland.enable = lib.mkEnableOption "Enable Neko's Hyprland NixOS Config";
   };
   config = lib.mkIf config.neko.hyprland.enable {
-    nixos.programs.hyprland = {
-        enable = true;
-        package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-        withUWSM = true;
-        xwayland.enable = true;
+    programs.hyprland = {
+      enable = true;
+      package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+      withUWSM = true;
+      xwayland.enable = true;
     };
   };
 }
