b920e689fa9fd850466d25bcc4f0379c7e6df219
bndlfm <firefliesandlightningbugs@gmail.com>
2025-03-12 03:46:37 -0500

    adding substituter for cosmic

--- flake.nix
+++ flake.nix
@@ -142,6 +142,10 @@
             ## WINDOW MANAGERS
               cosmic.nixosModules.default
               {
+                nix.settings = {
+                  substituters = [ "https://cosmic.cachix.org/" ];
+                  trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+                };
                 services.desktopManager.cosmic.enable = true;
               }
               niri.nixosModules.niri
