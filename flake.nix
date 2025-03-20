a62a73efbe5d475e755bcf1225ff897f92d96a2e
bndlfm <firefliesandlightningbugs@gmail.com>
2025-03-20 14:41:12 -0500

    post reinstall bs cause weird behaviour

--- flake.nix
+++ flake.nix
@@ -39,6 +39,14 @@
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
   };
 
+  nixConfig = {
+    extra-subsituters = [
+      "https://cache.nixos.org"
+    ];
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    ];
+  };
 
   outputs =
     {
