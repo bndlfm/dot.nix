85cc8a0da9165f7676796a2f5bab0c1123bf97c6
bndlfm <firefliesandlightningbugs@gmail.com>
2026-03-01 20:43:40 -0600

    fixed niri's public key for cachix

--- cachix/niri.nix
+++ cachix/niri.nix
@@ -1,6 +1,6 @@
 {
   nix.settings = {
     substituters = [ "https://niri.cachix.org" ];
-    trusted-public-keys = [ "niri.cachix.org-1:Wv0OmO7PsuJ9OV2cw8rE8lF6cNfyGl6z1h28T+uDCN8=" ];
+    trusted-public-keys = [ "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964=" ];
   };
 }
