4401268fc3d3a2ca0d8e32e4c4bbfc6aabd77905
bndlfm <firefliesandlightningbugs@gmail.com>
2026-03-04 01:17:45 -0600

    trying to switch back to nvidia binary driver for vr

--- hosts/meow/default.nix
+++ hosts/meow/default.nix
@@ -412,13 +412,13 @@ in
     nvidia = {
       open = false;
       package = config.boot.kernelPackages.nvidiaPackages.latest;
-      #let
+      #FIX: delete
+      # let
       #  base = config.boot.kernelPackages.nvidiaPackages.latest;
       #  cachyos-nvidia-patch = pkgs.fetchpatch {
       #    url = "https://raw.githubusercontent.com/CachyOS/CachyOS-PKGBUILDS/master/nvidia/nvidia-utils/kernel-6.19.patch";
       #    sha256 = "sha256-YuJjSUXE6jYSuZySYGnWSNG5sfVei7vvxDcHx3K+IN4=";
       #  };
-
       #  # Patch the appropriate driver based on config.hardware.nvidia.open
       #  driverAttr = if config.hardware.nvidia.open then "open" else "bin";
       #in
